161d9de1a7841fdb80056f3d25aabbb9672d0b15
Artyom Shadon <tom31245@gmail.com>
2020-09-11 14:46:45 +0300

    Вторая практическая (упр 1 и 2)

---
+++ src/ru/mirea/ikbo20/pr2/TestBall.java
@@ -0,0 +1,10 @@
+package ru.mirea.ikbo20.pr2;
+
+public class TestBall {
+    public static void main(String[] args) {
+        Ball ball1 = new Ball(100, 100);
+        System.out.println(ball1);
+        ball1.move(30, 15);
+        System.out.println(ball1);
+    }
+}
