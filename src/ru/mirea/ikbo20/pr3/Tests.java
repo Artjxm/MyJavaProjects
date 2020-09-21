b4690ba3b34141d4ac26a525ee0077721e545025
Artyom Shadon <tom31245@gmail.com>
2020-09-22 00:20:03 +0300

    Третья практическая (упражнение 3)

--- src/ru/mirea/ikbo20/pr3/Tests.java
+++ src/ru/mirea/ikbo20/pr3/Tests.java
@@ -2,13 +2,52 @@ package ru.mirea.ikbo20.pr3;
 
 public class Tests {
     public static void main(String[] args) {
-        Circle a = new Circle(3, "red", true);
-        System.out.println(a);
+        Shape s1 = new Circle(5.5, "RED", false);
+        System.out.println(s1);
+        System.out.println(s1.getArea());
+        System.out.println(s1.getPerimeter());
+        System.out.println(s1.getColor());
+        System.out.println(s1.isFilled());
 
-        Rectangle b = new Rectangle(3, 4, "blue", true);
-        System.out.println(b);
+        Circle c1 = (Circle)s1;
+        System.out.println(c1);
+        System.out.println(c1.getArea());
+        System.out.println(c1.getPerimeter());
+        System.out.println(c1.getColor());
+        System.out.println(c1.isFilled());
+        System.out.println(c1.getRadius());
 
-        Square c = new Square(5, "orange", false);
-        System.out.println(c);
+//      Абстрактный класс не может иметь экземпляров (бан)
+//      Shape s2 = new Shape();
+
+        Shape s3 = new Rectangle(1.0, 2.0, "RED", false);
+        System.out.println(s3);
+        System.out.println(s3.getArea());
+        System.out.println(s3.getPerimeter());
+        System.out.println(s3.getColor());
+
+        Rectangle r1 = (Rectangle)s3;
+        System.out.println(r1);
+        System.out.println(r1.getArea());
+        System.out.println(r1.getColor());
+        System.out.println(r1.getLength());
+
+        Shape s4 = new Square(6.6D);
+        System.out.println(s4);
+        System.out.println(s4.getArea());
+        System.out.println(s4.getColor());
+
+        Rectangle r2 = (Rectangle)s4;
+        System.out.println(r2);
+        System.out.println(r2.getArea());
+        System.out.println(r2.getColor());
+        System.out.println(r2.getLength());
+
+        Square sq1 = (Square)r2;
+        System.out.println(sq1);
+        System.out.println(sq1.getArea());
+        System.out.println(sq1.getColor());
+        System.out.println(sq1.getSide());
+        System.out.println(sq1.getLength());
     }
 }
