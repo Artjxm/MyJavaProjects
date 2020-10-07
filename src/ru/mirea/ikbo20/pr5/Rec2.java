48b7c8eae39be9b10d9230c321c9eb5edd5b92fe
Artyom Shadon <tom31245@gmail.com>
2020-10-07 11:40:11 +0300

    Пятая практическая (№ 2)

--- src/ru/mirea/ikbo20/pr5/Rec2.java
+++ src/ru/mirea/ikbo20/pr5/Rec2.java
@@ -2,14 +2,18 @@ package ru.mirea.ikbo20.pr5;
 
 import java.util.Scanner;
 
-// Задание 12
-public class Rec1 {
+// Задание 13
+public class Rec2 {
+    private static int index = 0;
+
     public static int recursion() {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
+        ++index;
         if (num != 0) {
-            if (num % 2 != 0)
+            if (index % 2 != 0) {
                 System.out.println(num);
+            }
             recursion();
         }
         return 0;
