894020cadcb6cef91c89e4868a20411d56f7983a
ShadonPC <55762492+Shad0n@users.noreply.github.com>
2020-12-13 19:47:17 +0300

    ПР11 Задание 2

---
+++ src/ru/mirea/ikbo20/pr11/task2/Queue.java
@@ -0,0 +1,14 @@
+package ru.mirea.ikbo20.pr11.task2;
+
+public interface Queue {
+    void enqueue(Object element);
+    Object element();
+    Object dequeue();
+    Object remove();
+    Object peek();
+    void push(Object element);
+    int size();
+    boolean isEmpty();
+    void clear();
+    Object[] toArray();
+}
