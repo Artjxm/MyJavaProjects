eb48e9a6a20c81cc6f0bd7fad40f65c560132ee3
ShadonPC <55762492+Shad0n@users.noreply.github.com>
2020-12-13 11:35:45 +0300

    Модификация WaitList

---
+++ src/ru/mirea/ikbo20/pr8/UnfairWaitList.java
@@ -0,0 +1,12 @@
+package ru.mirea.ikbo20.pr8;
+
+public class UnfairWaitList<E> extends WaitList<E> {
+    public UnfairWaitList(){
+    }
+    public void remove(E element){
+        content.remove(element);
+    }
+    public void moveToBack(E element){
+        content.add(content.remove());
+    }
+}
