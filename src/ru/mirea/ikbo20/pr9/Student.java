e2b3804d3e56f8bbceb1cc21511276761fa58224
ShadonPC <55762492+Shad0n@users.noreply.github.com>
2020-12-13 11:46:00 +0300

    ПР9 - Модернизация прошлой лабы со студентами

---
+++ src/ru/mirea/ikbo20/pr9/Student.java
@@ -0,0 +1,30 @@
+package ru.mirea.ikbo20.pr9;
+
+public class Student implements Comparable<Student> {
+    private String name;
+    private int grade;
+
+    public int compareTo(Student o){
+        return Integer.compare(this.grade, o.getGrade());
+    }
+    public Student(String name, int grade){
+        this.name = name;
+        this.grade = grade;
+    }
+
+    private int getGrade() {
+        return grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+}
---
+++ src/ru/mirea/ikbo20/pr9/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.mirea.ikbo20.pr9;
+
+public class StudentNotFoundException extends Throwable {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
